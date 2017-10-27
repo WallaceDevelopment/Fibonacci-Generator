e51bad2000ac152df37baaf29e56dda96f6c3762
Danielle O'Sullivan <danielleos@users.noreply.github.com>
2017-10-27 15:14:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ def FibGen(number):
       FibList.append(curr) # Adds new current number to list
       prev2 = prev1 # Updates last two numbers
       prev1 = curr
-      i = i+1 # Aligns the iteration
-      continue
     else:
       curr = prev2 + prev1 # Updates current number
       FibList.append(curr) # Adds new current number to list
@@ -24,26 +22,6 @@ def FibGen(number):
       prev1 = curr # Updates last two numbers
   
   print FibList
-  
-  while True:
-    try:
-      # Attempt to change string input to integer input
-      number = int(raw_input("How many Fibonacci numbers do you want to see?"))
-    except ValueError:
-      # If invalid input is received
-      print "Sorry, I did not understand that input, please input a whole number."
-      # Restarts loop
-      continue
-    if number <= 0:
-      # Need to check if number is negative
-      print "Sorry, seems like that is not a positive integer, try again please."
-      continue
-    else:
-      # Successful parse of input
-      print "Producing %s Fibonacci numbers..." % number
-      # Calls Fibonacci Generator function
-      FibGen(number)
-      break
   
 
 while True:
